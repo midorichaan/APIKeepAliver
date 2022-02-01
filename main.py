13ba7fecd547f1a47602f982297e7ba33554218a
midorichaan <55083684+midorichaan@users.noreply.github.com>
2022-02-01 14:49:12 +0900

    [Fix] ping forever

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ loop = asyncio.get_event_loop()
 
 #async func
 async def ping_server():
+    print("Pinging...")
     async with aiohttp.ClientSession() as session:
         async with session.request("GET", "https://api.midorichan.cf/v1/") as request:
             try:
@@ -20,6 +21,23 @@ async def ping_server():
                     print("Data: " + str(data))
                     print("something went wrong. Status: " + str(data["code"]))
 
+#loop
+async def ping_server_forever():
+    while True:
+        async with aiohttp.ClientSession() as session:
+            async with session.request("GET", "https://api.midorichan.cf/v1") as request:
+                try:
+                    data = await request.json()
+                except:
+                    data = await request.text()
+                    print(f"Fetched: {data}")
+                else:
+                    print(f"Fetched: {data}")
+        await asyncio.sleep(1500)
+
 if __name__ == "__main__":
-    print("Pinging...")
-    loop.run_until_complete(ping_server())
+    while True:
+        loop.run_until_complete(ping_server())
+        loop.run_until_complete(asyncio.sleep(1500))
+    #print("Pinging...")
+    #loop.run_until_complete(ping_server())
